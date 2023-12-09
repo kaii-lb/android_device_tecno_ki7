50395a5fa4e67846b87484fc77a0095db88df6b2
kaii <70664258+kaii-lb@users.noreply.github.com>
2023-12-09 19:49:26 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_spark10pro-user
-add_lunch_combo omni_spark10pro-userdebug
-add_lunch_combo omni_spark10pro-eng
+# add_lunch_combo omni_spark10pro-user
+# add_lunch_combo omni_spark10pro-userdebug
+# add_lunch_combo omni_spark10pro-eng
